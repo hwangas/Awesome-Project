aad05b62ca59eb7f993f99383d10002633c2a879
Anthony Lew <anthony.lew@vanderbilt.edu>
2015-03-21 00:59:23 -0500

    Added Frag

--- pizzadreams/gen/com/example/pizzadreams/R.java
+++ pizzadreams/gen/com/example/pizzadreams/R.java
@@ -26,10 +26,12 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int fragment_friend=0x7f030001;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
+        public static final int hello_blank_fragment=0x7f050004;
         public static final int hello_world=0x7f050001;
         public static final int title_activity_main=0x7f050003;
     }
